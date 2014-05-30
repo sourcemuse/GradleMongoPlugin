66754dd53a91acfb96297703481be2416b6d3bf5
Robert Taylor <robert.taylor426@gmail.com>
2014-05-30 12:47:09 +0100

    Improved logging support, adding configuration tests. Closing #1.

--- src/main/groovy/com/sourcemuse/gradle/plugin/GradleMongoPlugin.groovy
+++ src/main/groovy/com/sourcemuse/gradle/plugin/GradleMongoPlugin.groovy
@@ -1,20 +1,30 @@
 package com.sourcemuse.gradle.plugin
 
+import static com.google.common.base.Charsets.UTF_8
+import static com.sourcemuse.gradle.plugin.LogDestination.CONSOLE
+import static com.sourcemuse.gradle.plugin.LogDestination.FILE
+import static com.sourcemuse.gradle.plugin.LogDestination.NONE
+import static com.sourcemuse.gradle.plugin.ManageProcessInstruction.CONTINUE_MONGO_PROCESS_WHEN_BUILD_PROCESS_STOPS
+import static com.sourcemuse.gradle.plugin.ManageProcessInstruction.STOP_MONGO_PROCESS_WHEN_BUILD_PROCESS_STOPS
+
 import de.flapdoodle.embed.mongo.Command
 import de.flapdoodle.embed.mongo.MongodExecutable
 import de.flapdoodle.embed.mongo.MongodStarter
 import de.flapdoodle.embed.mongo.config.IMongodConfig
 import de.flapdoodle.embed.mongo.config.MongodConfigBuilder
+import de.flapdoodle.embed.mongo.config.MongodProcessOutputConfig
 import de.flapdoodle.embed.mongo.config.Net
 import de.flapdoodle.embed.mongo.config.RuntimeConfigBuilder
 import de.flapdoodle.embed.mongo.distribution.Version
 import de.flapdoodle.embed.mongo.runtime.Mongod
+import de.flapdoodle.embed.process.config.io.ProcessOutput
+import de.flapdoodle.embed.process.io.IStreamProcessor
+import de.flapdoodle.embed.process.io.NamedOutputStreamProcessor
+import de.flapdoodle.embed.process.io.NullProcessor
 import de.flapdoodle.embed.process.runtime.Network
+import org.gradle.api.GradleScriptException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-
-import static com.sourcemuse.gradle.plugin.ManageProcessInstruction.CONTINUE_MONGO_PROCESS_WHEN_BUILD_PROCESS_STOPS
-import static com.sourcemuse.gradle.plugin.ManageProcessInstruction.STOP_MONGO_PROCESS_WHEN_BUILD_PROCESS_STOPS
 
 class GradleMongoPlugin implements Plugin<Project> {
 
@@ -69,7 +79,14 @@ class GradleMongoPlugin implements Plugin<Project> {
                 .net(new Net(pluginExtension.bindIp, pluginExtension.port, Network.localhostIsIPv6()))
                 .build();
 
-        MongodStarter runtime = MongodStarter.getInstance(new RuntimeConfigBuilder().defaults(Command.MongoD).daemonProcess(manageProcessInstruction == STOP_MONGO_PROCESS_WHEN_BUILD_PROCESS_STOPS).build());
+
+        def runtimeConfig = new RuntimeConfigBuilder()
+                .defaults(Command.MongoD)
+                .processOutput(getLogger(pluginExtension))
+                .daemonProcess(manageProcessInstruction == STOP_MONGO_PROCESS_WHEN_BUILD_PROCESS_STOPS)
+                .build()
+
+        MongodStarter runtime = MongodStarter.getInstance(runtimeConfig);
 
         MongodExecutable mongodExecutable = runtime.prepare(mongodConfig);
         mongodExecutable.start();
@@ -84,6 +101,34 @@ class GradleMongoPlugin implements Plugin<Project> {
     private void stopMongoDb(Project project) {
         Mongod.sendShutdown(InetAddress.getLoopbackAddress(), project."$PLUGIN_EXTENSION_NAME".port)
     }
+
+    private ProcessOutput getLogger(GradleMongoPluginExtension pluginExtension) {
+        def logDestination = pluginExtension.logging.toUpperCase() as LogDestination
+
+        if (logDestination == CONSOLE) {
+            return MongodProcessOutputConfig.getDefaultInstance(Command.MongoD)
+        }
+
+        if (logDestination == FILE) {
+            def fileOutputStreamProcessor = new FileOutputStreamProcessor(pluginExtension.logFilePath)
+
+            return new ProcessOutput(
+                    new NamedOutputStreamProcessor('[mongod output]', fileOutputStreamProcessor),
+                    new NamedOutputStreamProcessor('[mongod error]', fileOutputStreamProcessor),
+                    new NamedOutputStreamProcessor('[mongod commands]', fileOutputStreamProcessor));
+        }
+
+        if (logDestination == NONE) {
+            def nullProcessor = new NullProcessor()
+            return new ProcessOutput(nullProcessor, nullProcessor, nullProcessor)
+        }
+
+        throw new GradleScriptException(
+                "Unrecognized 'logging' option: ${pluginExtension.logging}. " +
+                        "Choose one of ${LogDestination.values().collect { it.toString().toLowerCase() }.join(', ')}.",
+                new IllegalArgumentException()
+        )
+    }
 }
 
 enum ManageProcessInstruction {
@@ -91,6 +136,33 @@ enum ManageProcessInstruction {
 }
 
 class GradleMongoPluginExtension {
-    def port = 27017
-    def bindIp = '127.0.0.1'
+    int port = 27017
+    String bindIp = '127.0.0.1'
+    String logging = CONSOLE as String
+    String logFilePath = 'embedded-mongo.log'
+}
+
+enum LogDestination {
+    CONSOLE, FILE, NONE
+}
+
+class FileOutputStreamProcessor implements IStreamProcessor {
+
+    File logFile
+
+    FileOutputStreamProcessor(String filePath) {
+        logFile = new File(filePath)
+        logFile.mkdirs()
+        logFile.createNewFile()
+    }
+
+    @Override
+    void process(String block) {
+        logFile.text += block
+    }
+
+    @Override
+    void onProcessed() {
+
+    }
 }
--- src/test/groovy/com/sourcemuse/gradle/plugin/GradleMongoPluginSpec.groovy
+++ src/test/groovy/com/sourcemuse/gradle/plugin/GradleMongoPluginSpec.groovy
@@ -1,5 +1,11 @@
 package com.sourcemuse.gradle.plugin
 
+import static com.sourcemuse.gradle.plugin.GradleMongoPluginSpec.BuildScriptBuilder.buildScript
+import static com.sourcemuse.gradle.plugin.GradleMongoPluginSpec.DEFAULT_MONGOD_PORT
+import static com.sourcemuse.gradle.plugin.TestPlugin.TEST_START_MANAGED_MONGO_DB
+import static com.sourcemuse.gradle.plugin.TestPlugin.TEST_START_MONGO_DB
+import static com.sourcemuse.gradle.plugin.TestPlugin.TEST_STOP_MONGO_DB
+
 import com.mongodb.MongoClient
 import de.flapdoodle.embed.mongo.runtime.Mongod
 import org.gradle.api.Plugin
@@ -14,25 +20,21 @@ import static com.sourcemuse.gradle.plugin.GradleMongoPluginSpec.mongoInstanceRu
 
 class GradleMongoPluginSpec extends Specification {
 
+    static final DEFAULT_MONGOD_PORT = 27017
+
     @Rule TemporaryFolder tmp
     def gradleRunner = GradleRunnerFactory.create()
-
-    def setup() {
-        tmp.newFile('build.gradle') << """
-            apply plugin: ${GradleMongoPlugin.name}
-            apply plugin: ${TestPlugin.name}
-        """
-        gradleRunner.directory = tmp.root
-    }
+    def buildScript = new BuildScriptBuilder()
 
     def 'startManagedMongoDb starts a mongo instance, and then stops once the build has completed'() {
         given:
-        gradleRunner.arguments << 'testStartManagedMongoDb'
+        generate(buildScript)
+        gradleRunner.arguments << TEST_START_MANAGED_MONGO_DB
 
         when:
         ExecutionResult result = gradleRunner.run()
         def mongoRunningDuringBuild = result.standardOutput.contains('mongo running!')
-        def mongoRunningAfterBuild = mongoInstanceRunning()
+        def mongoRunningAfterBuild = mongoInstanceRunning(DEFAULT_MONGOD_PORT)
 
         then:
         mongoRunningDuringBuild
@@ -41,12 +43,13 @@ class GradleMongoPluginSpec extends Specification {
 
     def 'startMongoDb starts a mongo instance that continues running after the build has completed'() {
         given:
-        gradleRunner.arguments << 'testStartMongoDb'
+        generate(buildScript)
+        gradleRunner.arguments << TEST_START_MONGO_DB
 
         when:
         ExecutionResult result = gradleRunner.run()
         def mongoRunningDuringBuild = result.standardOutput.contains('mongo running!')
-        def mongoRunningAfterBuild = mongoInstanceRunning()
+        def mongoRunningAfterBuild = mongoInstanceRunning(DEFAULT_MONGOD_PORT)
 
         then:
         mongoRunningDuringBuild
@@ -55,47 +58,165 @@ class GradleMongoPluginSpec extends Specification {
 
     def 'stopMongoDb stops the mongo instance'() {
         given:
-        gradleRunner.arguments << 'testStopMongoDb'
+        generate(buildScript)
+        gradleRunner.arguments << TEST_STOP_MONGO_DB
 
         when:
-        ExecutionResult result = gradleRunner.run()
-        def mongoRunningAfterBuild = mongoInstanceRunning()
+        gradleRunner.run()
+        def mongoRunningAfterBuild = mongoInstanceRunning(DEFAULT_MONGOD_PORT)
 
         then:
         !mongoRunningAfterBuild
+    }
+
+    def 'port is configurable'() {
+        given:
+        generate(buildScript.withPort(12345))
+        gradleRunner.arguments << TEST_START_MONGO_DB
+
+        when:
+        gradleRunner.run()
+        def mongoRunningOnPort = mongoInstanceRunning(12345)
+
+        then:
+        mongoRunningOnPort
+    }
+
+    def 'logging can be switched off'() {
+        given:
+        generate(buildScript.withLogging("'none'"))
+        gradleRunner.arguments << TEST_START_MONGO_DB
+
+        when:
+        def executionResult = gradleRunner.run()
+
+        then:
+        !executionResult.standardOutput.contains('[mongod output]')
+    }
+
+    def 'logging can be routed to a file'() {
+        given:
+        def tempFile = tmp.newFile()
+        generate(buildScript.withLogging("'file'").withFilePath("'${tempFile.absolutePath}'"))
+        gradleRunner.arguments << TEST_START_MONGO_DB
+
+        when:
+        def executionResult = gradleRunner.run()
+
+        then:
+        !executionResult.standardOutput.contains('[mongod output]')
+        tempFile.text.contains('[mongod output]')
     }
 
     def cleanup() {
         ensureMongoIsStopped()
     }
 
-    def ensureMongoIsStopped() {
-        Mongod.sendShutdown(InetAddress.getLoopbackAddress(), 27017)
+    void ensureMongoIsStopped() {
+        Mongod.sendShutdown(InetAddress.getLoopbackAddress(), buildScript.port ?: DEFAULT_MONGOD_PORT)
     }
 
-    static boolean mongoInstanceRunning() {
+    static boolean mongoInstanceRunning(int port) {
         try {
-            MongoClient mongoClient = new MongoClient('localhost', 27017)
+            MongoClient mongoClient = new MongoClient('localhost', port)
             mongoClient.getDB('test').getStats()
         } catch (Exception e) {
             return false
         }
         true
     }
+
+    void generate(BuildScriptBuilder buildScriptBuilder) {
+        def buildScriptContent = buildScriptBuilder.build()
+        tmp.newFile('build.gradle') << buildScriptContent
+        gradleRunner.directory = tmp.root
+    }
+
+    static class BuildScriptBuilder {
+        int port
+        String logging
+        String filePath
+
+        String build() {
+            def mongoConfigBlock = new MongoPluginConfigBlock()
+
+            if (port) {
+                mongoConfigBlock.addPropertyConfig('port', "$port")
+            }
+
+            if (logging) {
+                mongoConfigBlock.addPropertyConfig('logging', logging)
+            }
+
+            if (filePath) {
+                mongoConfigBlock.addPropertyConfig('logFilePath', filePath)
+            }
+
+            """
+            |apply plugin: ${GradleMongoPlugin.name}
+            |apply plugin: ${TestPlugin.name}
+            |
+            |${mongoConfigBlock}
+            """.stripMargin()
+        }
+
+        static BuildScriptBuilder buildScript() {
+            new BuildScriptBuilder()
+        }
+
+        BuildScriptBuilder withPort(int port) {
+            this.port = port
+            this
+        }
+
+        BuildScriptBuilder withLogging(String logging) {
+            this.logging = logging
+            this
+        }
+
+        BuildScriptBuilder withFilePath(String filePath) {
+            this.filePath = filePath
+            this
+        }
+    }
+
+    static class MongoPluginConfigBlock {
+        String config = ''
+
+        void addPropertyConfig(String propertyName, String value) {
+            if (!config) {
+                config += 'mongo {\n'
+            }
+
+            config += "\t$propertyName=$value\n"
+        }
+
+        String toString() {
+            if (config) {
+                config += '}'
+            }
+        }
+    }
 }
 
+
 class TestPlugin implements Plugin<Project> {
+
+    static final TEST_STOP_MONGO_DB = 'testStopMongoDb'
+    static final TEST_START_MONGO_DB = 'testStartMongoDb'
+    static final TEST_START_MANAGED_MONGO_DB = 'testStartManagedMongoDb'
+
     @Override
     void apply(Project project) {
-        project.task(dependsOn: 'startManagedMongoDb', 'testStartManagedMongoDb') << {
-            if (mongoInstanceRunning())
+        project.task(dependsOn: 'startManagedMongoDb', TEST_START_MANAGED_MONGO_DB) << {
+            if (mongoInstanceRunning(DEFAULT_MONGOD_PORT))
                 println 'mongo running!'
         }
-        project.task(dependsOn: 'startMongoDb', 'testStartMongoDb') << {
-            if (mongoInstanceRunning())
+        project.task(dependsOn: 'startMongoDb', TEST_START_MONGO_DB) << {
+            if (mongoInstanceRunning(DEFAULT_MONGOD_PORT))
                 println 'mongo running!'
         }
-        project.task(dependsOn: ['startMongoDb', 'stopMongoDb'], 'testStopMongoDb')
+        project.task(dependsOn: ['startMongoDb', 'stopMongoDb'], TEST_STOP_MONGO_DB)
     }
 }
 
