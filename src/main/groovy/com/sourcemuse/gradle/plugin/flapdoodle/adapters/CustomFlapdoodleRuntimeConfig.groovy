0d7aa046f20a588c216af5a7b9e425d44b285b03
Robert Taylor <robert.taylor426@gmail.com>
2016-01-07 20:47:31 +0000

    Revert "Further simplification."
    This reverts commit 0cf9c24784ce986ddcc2f8ca72dd11b4c444ada2.

--- src/main/groovy/com/sourcemuse/gradle/plugin/flapdoodle/adapters/CustomFlapdoodleRuntimeConfig.groovy
+++ src/main/groovy/com/sourcemuse/gradle/plugin/flapdoodle/adapters/CustomFlapdoodleRuntimeConfig.groovy
@@ -3,7 +3,6 @@ import de.flapdoodle.embed.mongo.Command
 import de.flapdoodle.embed.mongo.config.ArtifactStoreBuilder
 import de.flapdoodle.embed.mongo.config.DownloadConfigBuilder
 import de.flapdoodle.embed.mongo.config.RuntimeConfigBuilder
-import de.flapdoodle.embed.process.config.store.IDownloadConfig
 import de.flapdoodle.embed.process.distribution.Distribution
 import de.flapdoodle.embed.process.distribution.IVersion
 import de.flapdoodle.embed.process.runtime.ICommandLinePostProcessor
@@ -13,24 +12,23 @@ class CustomFlapdoodleRuntimeConfig extends RuntimeConfigBuilder {
     private final String mongodVerbosity
     private final String downloadUrl
 
-    CustomFlapdoodleRuntimeConfig(IVersion version, String mongodVerbosity, String userSuppliedDownloadUrl) {
+    CustomFlapdoodleRuntimeConfig(IVersion version, String mongodVerbosity, String downloadUrl) {
         this.version = version
         this.mongodVerbosity = mongodVerbosity
-        this.downloadUrl = determineDownloadUrl(userSuppliedDownloadUrl)
-    }
-
-    static String determineDownloadUrl(String s) {
-        s ?: new DownloadConfigBuilder().build().downloadPath
+        this.downloadUrl = downloadUrl
     }
 
     @Override
     RuntimeConfigBuilder defaults(Command command) {
         super.defaults(command)
 
-        IDownloadConfig downloadConfig = new DownloadConfigBuilder()
-                .defaultsForCommand(command)
-                .progressListener(new CustomFlapdoodleProcessLogger(version))
-                .downloadPath(downloadUrl).build()
+        DownloadConfigBuilder downloadConfigBuilder = new DownloadConfigBuilder()
+        downloadConfigBuilder.defaultsForCommand(command)
+                             .progressListener(new CustomFlapdoodleProcessLogger(version))
+
+        if (downloadUrl) {
+            downloadConfigBuilder.downloadPath(downloadUrl)
+        }
 
         commandLinePostProcessor(new ICommandLinePostProcessor() {
             @Override
@@ -40,7 +38,7 @@ class CustomFlapdoodleRuntimeConfig extends RuntimeConfigBuilder {
             }
         })
 
-        artifactStore().overwriteDefault(new ArtifactStoreBuilder().defaults(command).download(downloadConfig).build())
+        artifactStore().overwriteDefault(new ArtifactStoreBuilder().defaults(command).download(downloadConfigBuilder).build())
 
         this
     }
